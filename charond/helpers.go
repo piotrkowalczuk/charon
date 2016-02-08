33ac0a3900d7018efcdc81298860a06e1ee3dd83
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2016-02-08 01:31:08 +0100

    untouched helper function

--- charond/helpers.go
+++ charond/helpers.go
@@ -17,3 +17,16 @@ func nilBool(nb *nilt.Bool) nilt.Bool {
 
 	return *nb
 }
+
+func untouched(given, created, removed int64) int64 {
+	switch {
+	case given < 0:
+		return -1
+	case given == 0:
+		return -2
+	case given < created:
+		return 0
+	default:
+		return given - created
+	}
+}
---
+++ charond/helpers_test.go
@@ -0,0 +1,53 @@
+package main
+
+import "testing"
+
+func TestUntouched(t *testing.T) {
+	data := []struct {
+		given, created, removed, untouched int64
+	}{
+		{
+			given:     -1000,
+			created:   2,
+			removed:   10,
+			untouched: -1,
+		},
+		{
+			given:     0,
+			created:   0,
+			removed:   0,
+			untouched: -2,
+		},
+		{
+			given:     5,
+			created:   3,
+			removed:   0,
+			untouched: 2,
+		},
+		{
+			given:     10,
+			created:   0,
+			removed:   0,
+			untouched: 10,
+		},
+		{
+			given:     100,
+			created:   100,
+			removed:   100,
+			untouched: 0,
+		},
+		{
+			given:     5,
+			created:   0,
+			removed:   100,
+			untouched: 5,
+		},
+	}
+
+	for _, d := range data {
+		u := untouched(d.given, d.created, d.removed)
+		if u != d.untouched {
+			t.Errorf("wrong value, expected %d but got %d", d.untouched, u)
+		}
+	}
+}
