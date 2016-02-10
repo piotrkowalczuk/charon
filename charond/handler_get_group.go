a96547661b2ba49482f1a66ec99aac63c139564e
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2016-02-10 22:15:43 +0100

    get group handler missing logic

--- charond/handler_get_group.go
+++ charond/handler_get_group.go
@@ -16,6 +16,14 @@ type getGroupHandler struct {
 func (ggh *getGroupHandler) handle(ctx context.Context, req *charon.GetGroupRequest) (*charon.GetGroupResponse, error) {
 	ggh.loggerWith("group_id", req.Id)
 
+	act, err := ggh.retrieveActor(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if err = ggh.firewall(req, act); err != nil {
+		return nil, err
+	}
+
 	entity, err := ggh.repository.group.FindOneByID(req.Id)
 	if err != nil {
 		if err == sql.ErrNoRows {
