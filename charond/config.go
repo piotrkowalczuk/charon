955524ec9838f313a8ed1995f78928a0b3d85430
swarman <s.warman@travelaudience.com>
2016-03-01 11:03:48 +0100

    Fixed version flag. Built new archive

--- charond/config.go
+++ charond/config.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-const VERSION="0.0.1"
+const VERSION="0.0.2"
 
 type configuration struct {
 	host      string
@@ -69,8 +69,8 @@ func (c *configuration) init() {
 func (c *configuration) parse() {
 
 	if !flag.Parsed() {
-		flag.Parse()
 		ver := flag.Bool("version", false, "Print version and exit")
+		flag.Parse()
 		if *ver {
 			fmt.Printf("%s", VERSION)
 			os.Exit(0)
