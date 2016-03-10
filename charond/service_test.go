f921b5dca5e8fecbba99e3641f2c2855bf57b7ff
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2016-03-10 16:22:16 +0100

    TestInitPrometheus

---
+++ charond/service_test.go
@@ -0,0 +1,16 @@
+// +build unit,!postgres
+
+package main
+
+import (
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func TestInitPrometheus(t *testing.T) {
+	_, err := initPrometheus("namespace", "subsystem", prometheus.Labels{"server": "travis-ci"})()
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err.Error())
+	}
+}
