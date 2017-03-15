c2f9ec9ddd9e58b9339288b4a284dbf39c0b67e7
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2017-03-15 19:27:01 +0100

    scripts/after_success.sh - go version condition removed

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -2,15 +2,13 @@
 
 bash <(curl -s https://codecov.io/bash)
 
-if [ $TRAVIS_GO_VERSION == "1.8" ]; then
-	docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-	if [ ! -z "$TRAVIS_TAG" ]; then
-		export VCS_REF=$TRAVIS_TAG
-		make publish
-	fi
-	if [ $TRAVIS_BRANCH == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-		export VERSION=latest
-		export VCS_REF=master
-		make publish
-	fi
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+if [ ! -z "$TRAVIS_TAG" ]; then
+    export VCS_REF=$TRAVIS_TAG
+    make publish
+fi
+if [ $TRAVIS_BRANCH == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    export VERSION=latest
+    export VCS_REF=master
+    make publish
 fi
