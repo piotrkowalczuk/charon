7ef842370b478a243eb99cf917fa4c4c6ee80c88
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2017-03-19 22:41:13 +0100

    refresh token - initial commit

--- .circleci/scripts/test.sh
+++ .circleci/scripts/test.sh
@@ -4,6 +4,7 @@
 
 set -e
 
+
 gotestsum --debug --junitfile results.xml -- -p=1 -count=1 -race -coverprofile=cover-source.out -covermode=atomic -v ./...
 cat cover-source.out | grep -v '.pb.go' > cover-step1.out
 cat cover-step1.out | grep -v '.pqt.go' > cover.out
