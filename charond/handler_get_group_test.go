24740ed56f9421bcd3c2025f2503abbf8f60f130
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2017-07-21 21:12:18 +0200

    TestGetGroupHandler_Get

--- charond/handler_get_group_test.go
+++ charond/handler_get_group_test.go
@@ -7,7 +7,47 @@ import (
 	"github.com/piotrkowalczuk/charon/charonrpc"
 	"github.com/piotrkowalczuk/charon/internal/model"
 	"github.com/piotrkowalczuk/charon/internal/session"
+	"github.com/piotrkowalczuk/ntypes"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+func TestGetGroupHandler_Get(t *testing.T) {
+	suite := &endToEndSuite{}
+	suite.setup(t)
+	defer suite.teardown(t)
+
+	ctx := testRPCServerLogin(t, suite)
+
+	cres := testRPCServerCreateGroup(t, suite, timeout(ctx), &charonrpc.CreateGroupRequest{
+		Name:        "name",
+		Description: ntypes.NewString("description"),
+	})
+
+	gres, err := suite.charon.group.Get(ctx, &charonrpc.GetGroupRequest{
+		Id: cres.Group.Id,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err.Error())
+	}
+	if gres.Group.Name != cres.Group.Name {
+		t.Errorf("wrong name, expected %s but got %s", cres.Group.Name, gres.Group.Name)
+	}
+	if gres.Group.Description != cres.Group.Description {
+		t.Errorf("wrong description, expected %s but got %s", cres.Group.Description, gres.Group.Description)
+	}
+	_, err = suite.charon.group.Get(ctx, &charonrpc.GetGroupRequest{
+		Id: 1000,
+	})
+	if err == nil {
+		t.Fatal("missing error")
+	}
+	if st, ok := status.FromError(err); ok {
+		if st.Code() != codes.NotFound {
+			t.Errorf("wrong error code, expected %s but got %s", codes.NotFound.String(), st.Code().String())
+		}
+	}
+}
 
 func TestGetGroupHandler_firewall_success(t *testing.T) {
 	data := []struct {
