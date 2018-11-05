6699ca9b2dc9a2341eccfb969c9211cbe0703c61
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2018-11-05 12:06:41 +0100

    circleci - initial commit #4

--- .circleci/scripts/generate.sh
+++ .circleci/scripts/generate.sh
@@ -3,19 +3,19 @@ PROTO_INCLUDE="-I=/usr/include -I=."
 
 case $1 in
     lint)
-        ${PROTOC} ${PROTO_INCLUDE} --lint_out=. ./mnemosynerpc/*.proto
+        ${PROTOC} ${PROTO_INCLUDE} --lint_out=. ./charonrpc/*.proto
         ;;
     python)
-        python -m grpc_tools.protoc ${PROTO_INCLUDE} --python_out=. --grpc_python_out=. ./mnemosynerpc/*.proto
+        python -m grpc_tools.protoc ${PROTO_INCLUDE} --python_out=. --grpc_python_out=. ./charonrpc/*.proto
         ;;
     java)
         rm -rf ./tmp/java
         mkdir -p ./tmp/java
-        ${PROTOC} ${PROTO_INCLUDE} --java_out=./tmp/java ./mnemosynerpc/*.proto
+        ${PROTOC} ${PROTO_INCLUDE} --java_out=./tmp/java ./charonrpc/*.proto
         ;;
     golang | go)
-        ${PROTOC} ${PROTO_INCLUDE} --go_out=plugins=grpc:${GOPATH}/src ./mnemosynerpc/*.proto
-        goimports -w ./mnemosynerpc
+        ${PROTOC} ${PROTO_INCLUDE} --go_out=plugins=grpc:${GOPATH}/src ./charonrpc/*.proto
+        goimports -w ./charonrpc
         ;;
 	*)
 	    echo "code generation failure due to unknown language: ${1}"
