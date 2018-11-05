9200ef87b9b3de038189e0555fe4436b36984247
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2018-11-05 12:09:02 +0100

    circleci - initial commit #5

--- .circleci/scripts/generate.sh
+++ .circleci/scripts/generate.sh
@@ -1,21 +1,22 @@
 : ${PROTOC:="/usr/local/bin/protoc"}
-PROTO_INCLUDE="-I=/usr/include -I=."
+SERVICE="charon"
+PROTO_INCLUDE="-I=./${SERVICE}rpc -I=/usr/include -I=./vendor/github.com/piotrkowalczuk"
 
 case $1 in
     lint)
-        ${PROTOC} ${PROTO_INCLUDE} --lint_out=. ./charonrpc/*.proto
+        ${PROTOC} ${PROTO_INCLUDE} --lint_out=. ./${SERVICE}rpc/*.proto
         ;;
     python)
-        python -m grpc_tools.protoc ${PROTO_INCLUDE} --python_out=. --grpc_python_out=. ./charonrpc/*.proto
+        python -m grpc_tools.protoc ${PROTO_INCLUDE} --python_out=. --grpc_python_out=. ./${SERVICE}rpc/*.proto
         ;;
     java)
         rm -rf ./tmp/java
         mkdir -p ./tmp/java
-        ${PROTOC} ${PROTO_INCLUDE} --java_out=./tmp/java ./charonrpc/*.proto
+        ${PROTOC} ${PROTO_INCLUDE} --java_out=./tmp/java ./${SERVICE}rpc/*.proto
         ;;
     golang | go)
-        ${PROTOC} ${PROTO_INCLUDE} --go_out=plugins=grpc:${GOPATH}/src ./charonrpc/*.proto
-        goimports -w ./charonrpc
+        ${PROTOC} ${PROTO_INCLUDE} --go_out=plugins=grpc:${GOPATH}/src ./${SERVICE}rpc/*.proto
+        goimports -w ./${SERVICE}rpc
         ;;
 	*)
 	    echo "code generation failure due to unknown language: ${1}"
